035c22c7ed863bf19eb1f1ba54bcbae9858b1e18
Mayinuddin Munna <mayinuddin.munna@gmaile.com>
2022-10-28 06:30:20 +0600

    minimal project setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+// require('dotenv').config();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors);
+app.use(express.json());
+
+async function run() {
+    try {
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("HEllO JUPI-MART");
+});
+
+app.listeners(port, () => {
+    console.log(`Server is running at ${port}`);
+})
